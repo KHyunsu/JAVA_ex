04811a647bb4eef7316486d6527824a2f3f235da
KHS <132638595+KHyunsu@users.noreply.github.com>
2025-03-05 14:53:23 +0900

    Add files via upload

---
+++ no2108.java
@@ -0,0 +1,55 @@
+package github;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+// 백준2108번
+public class no2108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        double sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+        int max = -1;
+        int mod = arr[0];
+        boolean check = false;
+
+        // 최빈값 구하기
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (max < count) {
+                max = count;
+                mod = arr[i];
+                check = true;
+            } else if (max == count && check == true) {
+                mod = arr[i];
+                check = false;
+            }
+        }
+        // 산술평균
+        System.out.println(Math.round(sum / N));
+        // 중앙값
+        System.out.println(arr[(N - 1) / 2]);
+        // 최빈값
+        System.out.println(mod);
+        // 범위
+        System.out.println(arr[N - 1] - arr[0]);
+    }
+}
